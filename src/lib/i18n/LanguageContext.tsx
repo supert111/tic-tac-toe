0c9022df78812770db31b6779a3a0aa95a81b9ad
supert111 <sportsmen@bigmir.net>
2025-08-30 18:26:55 +0300

    Change first lang en

--- src/lib/i18n/LanguageContext.tsx
+++ src/lib/i18n/LanguageContext.tsx
@@ -13,7 +13,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export function LanguageProvider({ children }: { children: React.ReactNode }) {
-  const [language, setLanguageState] = useState<Language>('uk');
+  const [language, setLanguageState] = useState<Language>('en');
 
   // Завантажити мову з localStorage при ініціалізації
   useEffect(() => {
